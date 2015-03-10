e72f06a5d247c8b5004990f4aa6b32af639d8486
Diego Schwindt <diego.sch21@gmail.com>
2015-03-10 13:06:18 -0300

    Symfony quickstart: pruebas doctrine

--- PHP/Symfony/quickstart/src/Diego/DemoBundle/Controller/DatabasePruebaController.php
+++ PHP/Symfony/quickstart/src/Diego/DemoBundle/Controller/DatabasePruebaController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Diego\DemoBundle\Entity\Product; 
+use Diego\DemoBundle\Entity\Category;
 
 class DatabasePruebaController extends Controller
 {
@@ -14,17 +15,28 @@ class DatabasePruebaController extends Controller
      */
     public function createAction()
     {
+        $category = new Category();
+        $category->setName('Main Products');
+
         $product = new Product();
         $product->setName('A Foo Bar');
         $product->setPrice('19.99');
         $product->setDescription('Lorem ipsum dolor');
+        // relate this product to the category
+        $product->setCategory($category);
 
         $em = $this->getDoctrine()->getManager();
-
+        $em->persist($category);
         $em->persist($product);
         $em->flush();
 
-        return new Response('Created product id '.$product->getId());
+        return $this->render(
+            'DiegoDemoBundle:Database:view.html.twig',
+            array(
+                'accion' => 'Created product',
+                'data' => 'Id: '.$product->getId().' Category id: '.$category->getId()
+            )
+        );
 
     }
 
@@ -33,18 +45,36 @@ class DatabasePruebaController extends Controller
      */
     public function showAction($id)
     {
+        // 1 query ahora, otra query despues cuando obtiene nombre de category
         $product = $this->getDoctrine()
             ->getRepository('DiegoDemoBundle:Product')
             ->find($id);
-
         if (!$product) {
             throw $this->createNotFoundException(
                 'No product found for id '.$id
             );
-        }
+        }    
 
-        // ... do something, like pass the $product object into a template
-        return new Response('Product id: '.$product->getId().' name: '.$product->getName().' price: '.$product->getPrice());
+        /* 1 query con JOIN (metodo en ProductRepository) */
+        // $product = $this->getDoctrine()
+        //         ->getRepository('DiegoDemoBundle:Product')
+        //         ->findOneByIdJoinedToCategory($id);
+
+        $category = $product->getCategory();  
+        echo get_class($category).'<br/>';
+        if ($category)  
+            $categoryName = $category->getName();   // Si use la priera alternativa acá vuelve a hacer query para obtener el nombre (lazy)
+        else                                        // Si use el JOIN, ya tiene esta data
+            $categoryName = "-";
+
+
+        return $this->render(
+            'DiegoDemoBundle:Database:view.html.twig',
+            array(
+                'accion' => 'Mostrando producto...',
+                'data' => 'Product id: '.$product->getId().' name: '.$product->getName().' price: '.$product->getPrice().' Category: '.$categoryName
+            )
+        );      
     }
 
     /**
@@ -62,10 +92,151 @@ class DatabasePruebaController extends Controller
         }
 
         $product->setName('New product name!');
+        $product->setPrice('52.569');
+
+        echo get_class($product).'<br/>';
+        
+        $category = $product->getCategory(); 
+        if (!$category || $category->getId(2)) {
+            $category = $em->getRepository('DiegoDemoBundle:Category')
+                ->find(2);
+            $product->setCategory($category);
+        }
+        $category->setName('Category updated by '.$id);
+
+        echo get_class($category).'<br/>';
+
+        //$em->persist($category);
         $em->flush();
 
-        return $this->redirect($this->generateUrl('diego_demo_homepage',array('limit' => 10)));
+        //return $this->redirect($this->generateUrl('diego_demo_homepage',array('limit' => 10)));
+        return $this->render(
+            'DiegoDemoBundle:Database:view.html.twig',
+            array(
+                'accion' => 'Cambio nombre y precio producto...',
+                'data' => 'Product id: '.$product->getId().' name: '.$product->getName().' Category name: '.$category->getName()
+            )
+        );
     }
+
+    /**
+     * @Route("/remove/{id}")
+     */
+    public function removeAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $product = $em->getRepository('DiegoDemoBundle:Product')->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+        $productid = $product->getId();
+        $em->remove($product);
+        $em->flush();
+
+        //return $this->redirect($this->generateUrl('diego_demo_homepage',array('limit' => 10)));
+        return $this->render(
+            'DiegoDemoBundle:Database:view.html.twig',
+            array(
+                'accion' => 'Remove product...',
+                'data' => 'Producto borrado id: '.$productid
+            )
+        );
+    }
+
+    /**
+     * @Route("/show")
+     */
+    public function showUpdatedAction()
+    {
+        $products = $this->getDoctrine()
+            ->getRepository('DiegoDemoBundle:Product')
+            ->findByName("New product name!");
+
+        $data = array();    
+        foreach ($products as $key => $product) {
+            array_push($data, 'Product id: '.$product->getId().' name: '.$product->getName().' price: '.$product->getPrice());    
+        }    
+
+        return $this->render(
+            'DiegoDemoBundle:Database:view.html.twig',
+            array(
+                'accion' => 'Mostrando productos con nombre actualizado...',
+                'data' => json_encode($data)
+            )
+        );      
+    }
+
+    /**
+     * @Route("/show_category")
+     */
+    public function showProductsCategoryUpdatedAction()
+    {
+        $category = $this->getDoctrine()
+               ->getRepository('DiegoDemoBundle:Category')
+               ->find(2);
+        echo get_class($category).'<br/>';       
+        $products = $category->getProducts();       
+        echo get_class($products).'<br/>';
+
+        $data = array();    
+        foreach ($products as $key => $product) {
+            array_push($data, 'Product id: '.$product->getId().' name: '.$product->getName().' price: '.$product->getPrice().' Category name: '.$category->getName());    
+        }    
+
+        return $this->render(
+            'DiegoDemoBundle:Database:view.html.twig',
+            array(
+                'accion' => 'Mostrando productos con categoria actualizada...',
+                'data' => json_encode($data)
+            )
+        );      
+    }
+
+    /**
+     * @Route("/show/price/{p}")
+     */
+    public function showPriceAction($p)
+    {
+        
+        /* USANDO QUERY BUILDER */
+        // $repository = $this->getDoctrine()->getRepository('DiegoDemoBundle:Product');
+        // $query = $repository->createQueryBuilder('p')
+        //     ->where('p.price >= :price')
+        //     ->setParameter('price', $p)
+        //     ->orderBy('p.price', 'ASC')
+        //     ->getQuery();
+        // $products = $query->getResult();
+        
+        /* USANDO DQL */
+        // $query = $this->getDoctrine()->getManager()->createQuery(
+        //     "SELECT p
+        //     FROM DiegoDemoBundle:Product p
+        //     WHERE p.price >= :price
+        //     ORDER BY p.price ASC"
+        // )->setParameter('price', $p);
+        // $products = $query->getResult();
+
+        /* USANDO funcion definida en ProductRepository */
+        $products = $this->getDoctrine()->getManager()
+            ->getRepository('DiegoDemoBundle:Product')->findAllMayorPrecio($p); 
+
+        $data = array();    
+        foreach ($products as $key => $product) {
+            array_push($data, 'Product id: '.$product->getId().' name: '.$product->getName().' price: '.$product->getPrice());    
+        }    
+
+        return $this->render(
+            'DiegoDemoBundle:Database:view.html.twig',
+            array(
+                'accion' => 'Mostrando productos con precio mayor a '.$p,
+                'data' => json_encode($data)
+            )
+        );      
+    }
+
 
 
 }
--- PHP/Symfony/quickstart/src/Diego/DemoBundle/Entity/Product.php
+++ PHP/Symfony/quickstart/src/Diego/DemoBundle/Entity/Product.php
@@ -8,7 +8,8 @@ use Doctrine\ORM\Mapping as ORM;
  * Product
  *
  * @ORM\Table()
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="Diego\DemoBundle\Entity\ProductRepository")
+ * @ORM\HasLifecycleCallbacks()
  */
 class Product
 {
@@ -42,6 +43,45 @@ class Product
      */
     private $description;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="updatedAt", type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Category", inversedBy="products")
+     * @ORM\JoinColumn(name="category_id", referencedColumnName="id")
+     */
+    protected $category;
+
+    /**
+     * @ORM\PrePersist
+     *
+     * Se ejecuta automaticamente antes de crear un elemento
+     */
+    public function setCreatedAtValue()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * @ORM\PreUpdate
+     *
+     * Se ejecuta automaticamente antes de crear hace update
+     */
+    public function setUpdatedAtValue()
+    {
+        $this->updatedAt = new \DateTime();
+    }
 
     /**
      * Get id
@@ -121,4 +161,28 @@ class Product
     {
         return $this->description;
     }
+
+    /**
+     * Set category
+     *
+     * @param \Diego\DemoBundle\Entity\Category $category
+     * @return Product
+     */
+    public function setCategory(\Diego\DemoBundle\Entity\Category $category = null)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \Diego\DemoBundle\Entity\Category 
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
 }
