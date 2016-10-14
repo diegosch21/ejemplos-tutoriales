bd8dee4d877a859bdf49c3a091b00d7d5478024d
Diego Schwindt <diego.sch21@gmail.com>
2016-10-14 12:52:10 -0300

    Ionic2 tutorial github ( https://scotch.io/tutorials/build-a-mobile-app-with-angular-2-and-ionic-2 ) stary y generacion de pages

---
+++ ionic2/githubIonic/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+@NgModule({
+  declarations: [
+    MyApp,
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+  ],
+  providers: []
+})
+export class AppModule {}
